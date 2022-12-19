dea0af849ef01c2e110fb8b05d751d500bfdfe41
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-12-19 20:00:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+scoreRightWrist = 0;
+ scoreLeftWrist = 0;
+  rightWristX = 0;
+   rightWristY = 0;
+    leftWristX = 0; 
+    leftWristY=0;
 var musica=""
 function preload(){
     musica=loadSound("music.mp3")
@@ -8,6 +14,8 @@ function setup(){
 
     video=createCapture(VIDEO)
     video.hide()
+    poseNet=ml5.poseNet(video,modelLoaded)
+    poseNet.on("pose",gotPoses)
 }
 function draw(){
     image(video,0,0,400,400)
@@ -15,4 +23,20 @@ function draw(){
 }
 function play(){
     musica.play()
+    musica.setVolume(1)
+    musica.rate(1)
+}
+function modelLoaded(){
+    console.log("modelo carregado")
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
+        scoreRightWrist=results[0].pose.keypoints[10].score
+        scoreLeftWrist=results[0].pose.keypoints[9].score
+        rightWristX=results[0].pose.rightWrist.x
+        rightWristY=results[0].pose.rightWrist.y
+        leftWristX=results[0].pose.leftWrist.x
+        leftWristY=results[0].pose.leftWrist.y 
+    }
 }
