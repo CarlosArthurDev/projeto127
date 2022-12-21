0101f61e12b6e57c0ca9e321c3829ff86401ba2a
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-12-21 17:49:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,13 +21,39 @@ function draw(){
     image(video,0,0,400,400)
     fill("#FF0000")
     stroke("#FF0000")
-    circle(leftWristX,leftWristY,20)
-    InNumberleftWristY=Number(leftWristY)
-    removeDecimais=floor(InNumberleftWristY)
-    volume=removeDecimais/500
-    document.getElementById("volume").innerHTML="volume="+volume
-    musica.setVolume(volume)
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX,leftWristY,20)
+        InNumberleftWristY=Number(leftWristY)
+        removeDecimais=floor(InNumberleftWristY)
+        volume=removeDecimais/500
+        document.getElementById("volume").innerHTML="volume="+volume
+        musica.setVolume(volume)
+    }
+    if(scoreRightWrist>0.2){
+        circle(rightWristX,rightWristY,20)
+        if(rightWristY>0 && rightWristY<=100){
+           document.getElementById("speed").innerHTML="velocidade=0.5"  
+           musica.rate(0.5)
+        }
+        else if(rightWristY>100 && rightWristY<=200){
+            document.getElementById("speed").innerHTML="velocidade=1"  
+            musica.rate(1)
+        }
+        else if(rightWristY>200 && rightWristY<=300){
+            document.getElementById("speed").innerHTML="velocidade=1.5"  
+            musica.rate(1.5)
+        }
+        else if(rightWristY>300 && rightWristY<=400){
+            document.getElementById("speed").innerHTML="velocidade=2"  
+            musica.rate(2)
+        }
+        else if(rightWristY>400 && rightWristY<=500){
+            document.getElementById("speed").innerHTML="velocidade=2.5"  
+            musica.rate(2.5)
+        }
+    }
 }
+    
 function tocar(){
     musica.play()
     musica.setVolume(1)
@@ -46,11 +72,4 @@ function gotPoses(results){
         leftWristX=results[0].pose.leftWrist.x
         leftWristY=results[0].pose.leftWrist.y
  }
-}
-ts[9].score
-        rightWristX=results[0].pose.rightWrist.x
-        rightWristY=results[0].pose.rightWrist.y
-        leftWristX=results[0].pose.leftWrist.x
-        leftWristY=results[0].pose.leftWrist.y 
-    }
 }
